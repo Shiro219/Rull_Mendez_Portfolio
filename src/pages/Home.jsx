560c6f1ba5e9c9d0c32eac25ac60721406611865
Shiro219 <rumendez@csumb.edu>
2026-06-14 18:08:32 -0700

    ThemeToggle implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
     return (
-        <div className= "min-h-screen ">
-
+        <div className= "min-h-screen bg-background text-foreground overflow-x-hidden ">
             Home
+            {}
+            <ThemeToggle/>
+            {}
+            {}
+            {}
+            
             
         </div>
-    )
+    );
 };
