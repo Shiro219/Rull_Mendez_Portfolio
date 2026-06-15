75e540bb18d319cb984ec080d7dd5761e583b258
Shiro219 <rumendez@csumb.edu>
2026-06-14 18:43:39 -0700

    Star animation added for the background (Dark Theme)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { ThemeToggle } from "../components/ThemeToggle";
-
+import { StarBackground } from "@/components/StarBackground";
 export const Home = () => {
     return (
         <div className= "min-h-screen bg-background text-foreground overflow-x-hidden ">
@@ -7,6 +7,7 @@ export const Home = () => {
             {}
             <ThemeToggle/>
             {}
+            <StarBackground/>
             {}
             {}
             
