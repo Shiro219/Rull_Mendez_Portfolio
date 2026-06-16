4eb94ccd2cb64a02d04851ae78d2d0f5908b2be6
Shiro219 <rumendez@csumb.edu>
2026-06-16 03:21:22 -0700

    AboutMe Section Added but needs information about me

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import { NavBar } from "../components/Navbar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+
+
+
 export const Home = () => {
     return (
         <div className= "min-h-screen bg-background text-foreground overflow-x-hidden ">
@@ -14,6 +18,7 @@ export const Home = () => {
             {}
             <main> 
                 <HeroSection/>
+                <AboutSection/>
             </main>
             
             
